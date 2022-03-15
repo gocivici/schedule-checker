1afe7dd07f8b735bc88f6bf1e9ab06cb142d839b
gocivici <gocivici@gmail.com>
2022-03-15 18:41:50 +0200

    changed localhost

--- app.py
+++ app.py
@@ -98,4 +98,5 @@ def index():
     return render_template("index.html", status=availableStatus, backColor=backColor, subText=subText)
     #st.markdown("<h1 style='text-align: center; color: white;'>{}</h1>".format(status), unsafe_allow_html=True)
 if __name__ == "__main__":
-    app.run(host='localhost', port=5000, debug=True)
+    #app.run(host='localhost', port=5000, debug=True)
+    app.run()
