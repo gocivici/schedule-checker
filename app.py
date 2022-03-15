43f7d3c475d8b839a8d9d90c6417e7a6e34e7375
gocivici <gocivici@gmail.com>
2022-03-15 23:56:19 +0200

    g

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def index():
     print(currentTime)
     #testDate = "2022-03-15T00:00:00Z"
     #url = "https://httpstat.us/200"
-    url = "http://tahvel.edu.ee/hois_back/timetableevents/timetableByRoom/31?from={}&room={}&thru={}".format(today,room,today)
+    url = "https://tahvel.edu.ee/hois_back/timetableevents/timetableByRoom/31?from={}&room={}&thru={}".format(today,room,today)
     print(url)
     #response = urlopen(url)
     # with urlopen(url) as response:
