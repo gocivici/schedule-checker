cce2c00352714920bd02d9fccbbc12dc9cc04e65
gocivici <gocivici@gmail.com>
2022-03-16 18:59:43 +0200

    utf

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def index():
     file = open('data.json')
 
 
-    data = json.loads(file.read().decode('UTF-8'))
+    data = json.loads(file.read())
 
     dayEvents = data['timetableEvents']
     #print(dayEvents)
